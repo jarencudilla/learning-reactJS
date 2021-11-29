7eed3f27d98684d03ac49c03adacf60d123f98bf
Jaren Cudilla <jarencudilla@gmail.com>
2021-11-29 19:09:17 +0800

    commit #4 building the react calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        <Button
+          className=""
+          value="0"
+          onClick={() => {
+            console.log("Button clicked!");
+          }}
+        />
+      </ButtonBox>
+    </Wrapper>
   );
-}
+};
 
 export default App;
+
