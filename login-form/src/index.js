301e4a115edd12a453ee9fffec622d521abee1b4
Jaren Cudilla <jarencudilla@gmail.com>
2021-12-01 19:36:33 +0800

    test coding

---
+++ login-form/src/index.js
@@ -0,0 +1,25 @@
+import Login from "./containers/Login";
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+// import ReactDOM from 'react-dom';
+
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const TestTable = () => {
+  return (
+    <div>
+        <div className='search-bar'>
+          <input type='text' />
+          <button>Search</button>
+        </div>
+    </div>
+  )
+
+}
