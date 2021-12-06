3a70f8971f3c9d0844198007e2024522493dce93
jarencudilla <jarencudilla@gmail.com>
2021-12-06 19:39:15 +0800

    updates codes based on current learnings

--- react-crud-app/src/listNames.jsx
+++ react-crud-app/src/listNames.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const ListNamesComponent = ({ names = [], onEdit, onDelete }) => {
+  const [filteredNames, setfilteredNames] = useState(names)
 
+  useEffect(() => {
+    setfilteredNames(names)
+  }, [names])
+  
   return (
     <ul>
-      {names.map((name) => (
+      {filteredNames.map((name) => (
         <li key={name.id} style={{ display: 'flex', gap: 10 }}>
           <div onClick={() => onEdit(name)}>
             {`${name.id} ${name.firstName} ${name.lastName}`}
