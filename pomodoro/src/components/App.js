8d621de59ab76849384a0c657d49b9cadff7f60a
jarencudilla <jarencudilla@gmail.com>
2021-12-08 16:45:09 +0800

    creates a pomodoro app as a coding exercise in ReactJS

---
+++ pomodoro/src/components/App.js
@@ -0,0 +1,28 @@
+<App />
+  <div>
+    <h1 />
+      <Timer />
+  <div>
+    <span> /TIMER/ </span>
+    <span> /CURRENT SESSION/ </span>
+  </div>
+<TimerControllers />
+  <div>
+    <WorkController />
+      <div>
+        <button> + </button>
+        <span> /BREAKTIME/ </span>
+        <button> - </button>
+      </div>
+    <BreakController />
+      <div>
+        <button> + </button>
+        <span> /WORKTIME/ </span>
+        <button> - </button>
+      </div>
+  </div>
+  <Sound />
+  <button>/SOUND ICON/</button>
+</div>
+
+export default App;
