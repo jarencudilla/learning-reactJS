edc84d3c178a4966441ea90050c0ad859bca7153
jarencudilla <jarencudilla@gmail.com>
2021-12-11 11:07:09 +0800

    updates on gameboard and adds inline styles

---
+++ tictactoe/src/components/board.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Box from './box'
+
+const style = {
+	width: "250px",
+	height: "250px",
+	margin: "0 auto",
+	display: "grid",
+	gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
+};
