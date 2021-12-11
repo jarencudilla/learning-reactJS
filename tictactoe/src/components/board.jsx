3d294a796d5bdf8012a432d970b26ce4255b7bd4
jarencudilla <jarencudilla@gmail.com>
2021-12-11 11:24:13 +0800

    adds code to draw board

--- tictactoe/src/components/board.jsx
+++ tictactoe/src/components/board.jsx
@@ -8,3 +8,11 @@ const style = {
 	display: "grid",
 	gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
 };
+
+const Board = (props) => (
+  <div style={style}>
+  {[ ...Array(9)].map((_, pos) => <Box key={pos} name={pos} onClick={()=>props.onClick(pos)} value={props.value[pos]}/>)}
+  </div>
+)
+
+export default Board
