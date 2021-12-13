7edf82703c6b41dc4e3887ad6a0b3b679c986504
jarencudilla <jarencudilla@gmail.com>
2021-12-13 19:21:48 +0800

    initial commit, setting up the new project app

---
+++ slackerish/src/App.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import Button from 'react-bootstrap/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
