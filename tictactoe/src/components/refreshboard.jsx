751b05dc6b5eef1d32b46952a00edbf885ee82e9
jarencudilla <jarencudilla@gmail.com>
2021-12-16 15:48:20 +0800

    Adds refresh board functionality

--- tictactoe/src/components/refreshboard.jsx
+++ tictactoe/src/components/refreshboard.jsx
@@ -1,1 +1,13 @@
-import react from 'react'
+import React from 'react'
+
+const style = {
+	width: "250px",
+	margin: "0 auto",
+	display: "grid",
+	fontSize: "50px",
+	fontWeight: "800",
+};
+
+const Refresh = (props) => <button name={"btn"} style={style} onClick={props.onClick}>{props.value}</button>
+
+export default Refresh
